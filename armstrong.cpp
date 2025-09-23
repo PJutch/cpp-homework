969941e6c9d3c3dce36d18d0bd2789fe5f49d2ec
PJutch <paveljutch@yandex.ru>
2025-09-23 11:40:37 +0300

    Upload armstrong numbers solution

---
+++ armstrong.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    for (int i = 1; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            for (int k = 0; k < 10; ++k) {
+                int number = i * 100 + j * 10 + k;
+                if (i * i * i + j * j * j + k * k * k == number) {
+                    std::cout << number << '\n';
+                }
+            }
+        }
+    }
+    return 0;
+}
