764d229cd4164e1a9a918d0bc4ef19a91153432c
PJutch <paveljutch@yandex.ru>
2025-10-01 00:50:10 +0300

    Implement Attorney-Client pattern

---
+++ 03_04_attorney_client.cpp
@@ -0,0 +1,42 @@
+#include <cassert>
+#include <iostream>
+
+class Entity {
+    friend class Attorney;
+
+    static void test1() { std::cout << "Test 1\n"; }
+
+    static void test2() { std::cout << "Test 2\n"; }
+
+    // ... lots of code ...
+
+    static void doNottest() { assert(false); }
+};
+
+class Attorney {
+    friend class Client1;
+    friend class Client2;
+
+    static void test1() { Entity::test1(); }
+
+    static void test2() { Entity::test2(); }
+};
+
+class Client1 {
+public:
+    static void test() { Attorney::test1(); }
+    
+    // ... lots of code ...
+};
+
+class Client2 {
+public:
+    static void test() { Attorney::test2(); }
+
+    // ... lots of code ...
+};
+
+int main() {
+    Client1::test();
+    Client2::test();
+}
